939db8c0d21f2970e3158ee525d2171d2e95ed38
Caroline-Nilsson <cn222nd@student.lnu.se>
2017-09-20 10:32:21 +0200

    Create flowchart for first assignment. add assembly files with comment head

---
+++ lab3/src/a2.asm
@@ -0,0 +1,28 @@
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+;   1DT301, Computer Technology I
+;   Date: YYYY-MM-DD
+;   Author:
+;                       Caroline Nilsson            (cn222nd)
+;                       Daniel Alm Grundström       (dg222dw)
+;
+;   Lab number:         
+;   Title:              
+;
+;   Hardware:           STK600, CPU ATmega2560
+;
+;   Function:           
+;
+;   Input ports:        
+;
+;   Output ports:       
+;
+;   Subroutines:        
+;   Included files:     m2560def.inc
+;
+;   Other information:  
+;
+;   Changes in program: 
+;                       
+;
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
