d337dc9ca22405ea664f56e1502b942bcfae30c0
Caroline-Nilsson <cn222nd@student.lnu.se>
2017-09-20 16:24:12 +0200

    Implement flowchart design for assignment 1

--- lab3/src/a1.asm
+++ lab3/src/a1.asm
@@ -26,3 +26,84 @@
 ;
 ;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
+.include "m2560def.inc"
+
+.def dataDir = r16
+.def ledState = r17
+.def xorComparison = r18
+
+.equ XOR_BIT_STRING = 0b0000_0001
+
+.cseg
+
+.org 0
+rjmp reset
+
+.org int0addr
+rjmp interrupt
+
+.org 0xF0 ;?
+reset: 
+	ldi dataDir, LOW(RAMEND)
+	out SPL, dataDir
+	ldi dataDir, HIGH(RAMEND)
+	out SPH, dataDir
+	
+	ser dataDir
+	out DDRB, dataDir
+
+	clr dataDir
+	out DDRD, dataDir
+
+	ser ledState
+	ldi xorComparison, XOR_BIT_STRING
+	
+	ldi dataDir, (1<<int0)
+	out GIMSK, dataDir
+	ldi dataDir, (2<<ICS00) 
+	out MCUCR, dataDir
+	
+	sei
+	
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+;
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+main_loop:
+	out PORTB, ledState
+	rjmp main_loop
+
+;----------------------------------------------------------------------
+;
+;----------------------------------------------------------------------
+
+interrupt:
+	rcall delay
+
+wait_release:
+	sbis PIND, PIND0
+		rjmp wait_release
+	
+	eor ledState, xorComparison
+	
+	reti
+	
+;----------------------------------------------------------------------
+;
+;----------------------------------------------------------------------	
+delay:
+	push ledState
+	push xorComparison
+	
+    ldi  r18, 13
+    ldi  r19, 252
+L1: dec  r19
+    brne L1
+    dec  r18
+    brne L1
+    nop
+	
+	pop xorComparison
+	pop ledState
+	
+	ret
