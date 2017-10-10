9d609e97a19b05c797f575696c35ccf9a7bc3385
Daniel Alm Grundström <daniel.alm.grundstrom@live.se>
2017-10-10 21:46:48 +0200

    Lab6: Creates library "display_utils" and implements ass. 2

--- lab6/a2.c
+++ lab6/a2.c
@@ -11,12 +11,14 @@ int main() {
     init_serial_comm(1 << TXEN1);
 #endif
     Frame info_frame = create_frame(Information);
-    strncpy(info_frame.line_1, "#", 1);         // info_frame.line_1 = "#"
+    strncpy(info_frame.line_1, "Hej", 3);
+    strncpy(info_frame.line_2, "pa", 2);
+    strncpy(info_frame.line_3, "dej", 3);
     set_checksum(&info_frame, calculate_checksum(&info_frame));
 
     Frame image_frame = create_frame(Image);
     set_checksum(&image_frame, calculate_checksum(&image_frame));
-    
+
     send_frame(&info_frame);
     send_frame(&image_frame);
 #ifndef DEBUG
